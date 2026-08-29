0e6729ce59a07ca3f752263872b86e2064b24b49
hartnettville55 <hartnett3527@student.faytechcc.edu>
2026-08-29 19:49:25 -0400

    P1HW1_HartnettTravis.py

--- P1HW1_HartnettTravis.py
+++ P1HW1_HartnettTravis.py
@@ -6,16 +6,16 @@
 print("-----Calculating Exponents-----\n\n")
 
 
-base = int(input("Enter an integer as the base value:"))
-exponent = int(input("Enter an integer as the exponent:"))
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
 result = base ** exponent 
 print(f"{base} raised to the power of {exponent} is {result} !!\n\n")
 
 
 print("-----Addition and Subtraction-----\n\n")
-start = int(input("Enter a starting integer:"))
-add = int(input("Enter an integer to add:"))
-subtract = int(input("Enter an integer to subtract:"))
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
 result_2 = start + add - subtract
 print(f"{start} + {add} - {subtract} is equal to {result_2}")
 
