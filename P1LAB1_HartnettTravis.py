6cd515070600eb2886bffdcdca8b693e9e365c2c
hartnettville55 <hartnett3527@student.faytechcc.edu>
2026-08-29 16:16:01 -0400

    Add files via upload

---
+++ P1LAB1_HartnettTravis.py
@@ -0,0 +1,9 @@
+# Travis Hartnett
+# 29 August 2026
+# P1LAB1
+# simple code designed to gather First and Last name and a brief sentence output using the users input
+
+
+first = input("Enter your first name:")
+last = input("Enter your last name:")
+print(f"Hello, {first} {last}! Welcome to CTI-110")
