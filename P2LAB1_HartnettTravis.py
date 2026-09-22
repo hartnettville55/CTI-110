fa5e228259f0ea11525f6046a4ab7015194f25f0
hartnettville55 <hartnett3527@student.faytechcc.edu>
2026-09-21 21:09:24 -0400

    Add files via upload

---
+++ P2LAB1_HartnettTravis.py
@@ -0,0 +1,17 @@
+# # Travis Hartnett
+# 21 August 2026
+# P2LAB1
+# The program will calculate the diameter, circumference, and area of a circle
+
+import math
+
+radius = float(input("What is the radius of the circle? "))
+print( )
+diameter = 2 * radius
+print(f"The diameter of the circle is {diameter:.1f}\n")
+
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle is {circumference:.2f}\n")
+
+area = math.pi * radius**2
+print(f"The area of the cirlce is {area:.3f}\n")
